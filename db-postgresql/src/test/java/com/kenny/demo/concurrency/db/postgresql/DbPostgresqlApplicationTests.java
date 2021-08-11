65d863ecbe736ecb528ec75adf692845198ca507
kenny.k <kenny.kerson@gmail.com>
2021-08-11 23:55:25 +0900

    # db-postgresql 모듈 추가

---
+++ db-postgresql/src/test/java/com/kenny/demo/concurrency/db/postgresql/DbPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.concurrency.db.postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
