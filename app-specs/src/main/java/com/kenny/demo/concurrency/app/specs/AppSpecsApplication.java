667bb04e43f5e05590d5eadb825203ced4fe548c
kenny.k <kenny.kerson@gmail.com>
2021-08-11 23:58:51 +0900

    # app-specs 모듈 신규 추가

---
+++ app-specs/src/main/java/com/kenny/demo/concurrency/app/specs/AppSpecsApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.concurrency.app.specs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpecsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpecsApplication.class, args);
+    }
+
+}
---
+++ app-specs/src/test/java/com/kenny/demo/concurrency/app/specs/AppSpecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.demo.concurrency.app.specs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpecsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
